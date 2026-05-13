8aeb75201628ba06cc5b1ae1aba013c2a338a6f5
enrollsys <enrollsys@kolabnow.com>
2026-05-13 23:27:47 +0400

    Add user registration and profile forms

--- core/forms.py
+++ core/forms.py
@@ -1,3 +1,24 @@
 from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from .models import UserProfile, Faculty, Department, StudyProgram, Application, ApplicationDocument, ExamResult, News, FAQ, Contact, DocumentTemplate
 
-# Forms will be added in subsequent commits
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+    first_name = forms.CharField(max_length=30, required=True, label='Имя')
+    last_name = forms.CharField(max_length=30, required=True, label='Фамилия')
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['role', 'phone']
+        widgets = {
+            'role': forms.Select(attrs={'class': 'form-control'}),
+            'phone': forms.TextInput(attrs={'class': 'form-control'}),
+        }
