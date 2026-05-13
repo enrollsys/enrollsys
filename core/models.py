a89f56097830c7504a0a0ecd726b2d86d90f77ea
enrollsys <enrollsys@kolabnow.com>
2026-05-13 23:25:10 +0400

    Add News model for university announcements

--- core/models.py
+++ core/models.py
@@ -190,3 +190,22 @@ class ExamResult(models.Model):
         verbose_name = 'Результат экзамена'
         verbose_name_plural = 'Результаты экзаменов'
         unique_together = ['application', 'exam']
+
+
+class News(models.Model):
+    title = models.CharField(max_length=300, verbose_name='Заголовок')
+    slug = models.SlugField(unique=True, verbose_name='URL-идентификатор')
+    content = models.TextField(verbose_name='Содержание')
+    image = models.ImageField(upload_to='news/%Y/%m/', blank=True, verbose_name='Изображение')
+    is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, verbose_name='Автор')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+        ordering = ['-created_at']
